e6fadd69677cbb12e82168cd204a634e10dbbefd
Oscar Andersson <oscar.andersson@kazoku.se>
2023-10-13 16:27:16 +0200

    mod

--- program4.py
+++ program4.py
@@ -1,4 +1,5 @@
 import itertools
+import json
 
 def xor_strings(s1, s2):
     """XOR two strings together and return the result."""
@@ -8,12 +9,8 @@ def crib_drag(combined_plaintexts, crib):
     """Attempt to drag the crib across the combined plaintexts (result of XORing two ciphertexts)."""
     results = []
     for i in range(len(combined_plaintexts) - len(crib) + 1):
-        # Extract a portion of the combined plaintexts of the same length as the crib
-        segment = combined_plaintexts[i:i+len(crib)]
-        
-        # XOR the segment with the crib
+        segment = combined_plaintexts[i:i+len(crib)] # Extract a portion of the combined plaintexts of the same length as the crib
         result = xor_strings(segment, crib)
-        
         results.append((i, result))
     return results
 
@@ -26,13 +23,22 @@ if __name__ == "__main__":
     c1 = ciphertexts[0]
     c2 = ciphertexts[1]
 
-    xor_combined_plaintexts = xor_strings(c1, c2)
-
     with open("1000eng.txt", 'r', encoding="ASCII") as file:
-        cribs: list[str] = [item.replace("\n", "") for item in file.readlines()]
+        common_words: list[str] = [item.replace("\n", "") for item in file.readlines()][:5]
 
-    for crib in cribs[:100]:
-        print("Starting crib dragging...\n")
-        for position, result in crib_drag(xor_combined_plaintexts, crib):
-            if is_eng(result):
-                print(f"{crib}\t{position}\t{result}")
+    results = {}
+    
+    # Iterate over all possible pairs of ciphertexts
+    for c1, c2 in itertools.combinations(ciphertexts, 2):
+        xor_combined_plaintexts = xor_strings(c1, c2)
+        for crib in common_words:
+            for position, result in crib_drag(xor_combined_plaintexts, crib):
+                if is_eng(result) and result in common_words:
+                    key = f"{c1}___XOR___{c2}"  # using the full ciphertexts
+                    if key not in results:
+                        results[key] = []
+                    results[key].append({"crib": crib, "position": position, "result": result})
+
+    # Dump results to a JSON object
+    with open("crib_drag_results.json", "w") as outfile:
+        json.dump(results, outfile, indent=4)
