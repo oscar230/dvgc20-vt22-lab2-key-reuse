b3970253a19b4fbb9b8c319fa2ff1d94e8e07a1e
Oscar Andersson <2kafkimc@duck.com>
2023-10-14 11:04:42 +0200

    mod

--- combine.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open("crib_drag_results.json", "r") as infile:
-    data = json.load(infile)
-
-print(data)
--- combine2.py
+++ combine2.py
@@ -1,44 +1,46 @@
 import json
+from typing import Union
 
-def display_results(json_path):
-    with open(json_path, 'r') as file:
-        data = json.load(file)
+def select_crib(data, position: int) -> Union[None, str]:
+    cribs_at_position = set()  # Using a set to automatically remove any duplicates
 
-    for key_data, results in data.items():
-        print("\nCiphertext pair:", key_data)
-        print("\nResults:")
-
-        for idx, result in enumerate(results):
-            print(f"{idx + 1}. Crib: {result['crib']}, Position: {result['position']}, Result: {result['result']}")
-
-def build_key_manually(json_path):
-    with open(json_path, 'r') as file:
-        data = json.load(file)
+    for _, results in data.items():
+        for result in results:
+            if result['position'] == position:
+                cribs_at_position.add(result['crib'])
     
-    selected_key_data = list(data.keys())[0]  # Selecting first pair as default for this example.
-    results = data[selected_key_data]
+    cribs = list(cribs_at_position)
+    if len(cribs) == 0:
+        return None
+    else:
+        for i, crib in enumerate(cribs, start=1):
+            if crib == ' ':
+                print(f"{i}. _SPACE_")
+            else:
+                print(f"{i}. {crib}")
 
-    key_length = max(result['position'] for result in results) + len(results[-1]['result'])
-    key = ['?'] * key_length
+        selected_crib: str = ''
+        while selected_crib not in cribs:
+            try:
+                selection_input: str = input(f"Select: ")
+                selected_crib = cribs[int(selection_input) - 1]
+            except:
+                print(f"Nope! Select between 1 and including {len(cribs)}")
 
-    while True:
-        display_results(json_path)
-        selection = input("\nEnter the number of the result you want to include in the key or 'q' to quit: ")
-
-        if selection.lower() == 'q':
-            break
-
-        try:
-            selected_result = results[int(selection) - 1]
-            position = selected_result['position']
-            for idx, char in enumerate(selected_result['result']):
-                key[position + idx] = char
-        except (ValueError, IndexError):
-            print("Invalid selection. Please enter a number from the list or 'q' to quit.")
-
-    return ''.join(key)
+        print(f"Selected: {selected_crib}")
+        return selected_crib
 
 if __name__ == "__main__":
     json_path = "crib_drag_results.json"
-    decryption_key = build_key_manually(json_path)
-    print("\nGenerated Decryption Key:", decryption_key)
+    with open(json_path, 'r') as file:
+        data = json.load(file)
+        key: str = ""
+        while True:
+            position: int = len(key)
+            print(f"\tKey:\t{key}\n\tPos:\t{position + 1}")
+            key_part: Union[None, str] = select_crib(data, position)
+            if not key_part:
+                print(key)
+                quit()
+            else:
+                key += key_part
