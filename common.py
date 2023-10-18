c06a19070ceaf9bc90a255a42eec1d5d171b54cb
Oscar Andersson <oscar.andersson@kazoku.se>
2023-10-18 10:04:57 +0200

    Try something new

--- common.py
+++ common.py
@@ -1,11 +1,23 @@
 from typing import Union
+import re
 
 CRIBRESULTFILE = "crib-results.json"
-CIPHERFILE = "attachment"
+CIPHERFILE = "attachment-easy"
 WORDFILE = "wordlist"
 RESULTSFILE = "results.json"
 JSONINDENT = 4
 ENCODING = "ASCII"
+
+def is_readable(text: str) -> bool:
+    # Count of allowed characters
+    allowed_count = len(re.findall(r'[A-Za-z0-9 ,."]', text))
+    # If more than a certain percentage of the characters are readable
+    return allowed_count / len(text) > 0.9
+# def is_readable(text: str) -> bool:
+#     readable_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ '#"!#$%&\'*+,-./?@\\^_`\n\r'
+#     return all(char in readable_chars for char in text)
+    # pattern = re.compile("^[a-zA-Z0-9?><;,{}[\]\-_+=!@#$%\^&*|']*$")
+    # return bool(re.match(pattern, text))
 
 def xor_strings(hex1, hex2):
     # XOR two HEX strings
